6617b04ed160ba742fbe25a4154739b0b30032e3
Stefan Podgorski <stefan.podgorski@adelaide.edu.au>
2024-02-02 15:33:24 +1030

    modified train.py  + split train function for splitting the mnist train set  refactored main to use train and val  + test set

--- mnist/train.py
+++ mnist/train.py
@@ -1,6 +1,13 @@
 import os
 import argparse
 from pathlib import Path
+from torchvision.datasets import MNIST
+import torchvision.transforms as transforms
+import torch.utils.data as data
+import torch
+import lightning as L  # Note it is no longer pytorch_lightning!
+from src.model import MNISTModel
+
 
 def parse_args():
     # Training settings
@@ -17,10 +24,50 @@ def parse_args():
                         help='random seed (default: 1)')
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
+    parser.add_argument('--dataset_path', type=str, default="DATASET",
+                        help='Point to where the dataset is')
     args = parser.parse_args()
     return args
 
+
+def split_train(dataset_full):
+    # use 20% of training data for validation
+    train_set_size = int(len(dataset_full) * 0.8)
+    valid_set_size = len(dataset_full) - train_set_size
+
+    # split the train set into two
+    seed = torch.Generator().manual_seed(42)
+    train_set, valid_set = data.random_split(dataset_full, [train_set_size, valid_set_size], generator=seed)
+    return train_set, valid_set
+
+
 def main(args):
+    model = MNISTModel()
+    # setup data
+    transform = transforms.ToTensor()
+    dataset_path = os.getenv(args.dataset_path)
+    dataset_full = MNIST(root=dataset_path,
+                         train=True, download=True, transform=transform)
+    dataset_train, dataset_val = split_train(dataset_full)
+    loader_train = data.DataLoader(dataset_train)
+    loader_val = data.DataLoader(dataset_val)
+
+    dataset_test = MNIST(root=dataset_path,
+                         train=False, download=True, transform=transform)
+    loader_test = data.DataLoader(dataset_test)
+
+    # train the model (hint: here are some helpful Trainer arguments for rapid idea iteration)
+    trainer = L.Trainer(devices=args.gpus,
+                        accelerator="gpu",
+                        strategy="deepspeed_stage_2",
+                        precision=16,
+                        max_epochs=args.epochs)
+
+    trainer.fit(model=model,
+                train_dataloaders=loader_train, valid_loader=loader_val)
+
+    trainer.test(model, dataloaders=loader_test)
+
     return
 
 
